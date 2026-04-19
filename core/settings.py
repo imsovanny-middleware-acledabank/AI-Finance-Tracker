008ab1c6e6bff9ff769ae00e6e5c5a81ac65a4f9
apple <imsovanny.appleid@gmail.cm>
2026-04-20 01:44:49 +0700

    Redesign login page, fix Render deploy config, update requirements

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") if os.environ.get("CSRF_TRUSTED_ORIGINS") else []
+CSRF_TRUSTED_ORIGINS += ["https://*.ngrok-free.dev"]
+
 
 # Application definition
 
